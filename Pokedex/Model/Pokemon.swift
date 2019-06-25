4b1e9e583b28f75d47dce580c04f9dd8f33e2210
julia rodriguez <julia@Trevors-MacBook-Air.local>
2019-06-25 13:23:48 -0600

    Initial Commit

---
+++ Pokedex/Model/Pokemon.swift
@@ -0,0 +1,42 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by julia rodriguez on 6/25/19.
+//  Copyright © 2019 julia rodriguez. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelDict: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case id
+        case abilities
+        case spritesDictionary = "spites"
+    }
+    
+    
+    let name: String
+    let id: Int
+    let abilities: [AbilitiesDictionary]
+    let spritesDictionary: SpritesDictionary
+}
+
+struct AbilitiesDictionary: Decodable {
+    let ability: Ability
+    struct Ability : Decodable {
+        let name: String
+      //let url: String
+    }
+}
+
+struct SpritesDictionary: Decodable {
+    let image: URL
+    
+    // CodingKeys - capitol C
+    private enum CodingKeys: String, CodingKey {
+        case image = "front_shiny"
+    }
+    
+}
